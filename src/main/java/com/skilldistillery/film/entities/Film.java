ab994baaeff4af542fada858e3643514deb0af4f
Richard Paleczny <palecznyr@gmail.com>
2018-08-17 15:45:08 -0600

    Made TODO comment in Film.java

--- src/main/java/com/skilldistillery/film/entities/Film.java
+++ src/main/java/com/skilldistillery/film/entities/Film.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.film.entities;
 
 public class Film {
+	// TODO - replace with Film class from homework.
 
 }
--- src/test/java/com/skilldistillery/film/entities/FilmTests.java
+++ src/test/java/com/skilldistillery/film/entities/FilmTests.java
@@ -7,7 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class FilmTests {
-
+	
 	@Before
 	public void setUp() throws Exception {
 	}
